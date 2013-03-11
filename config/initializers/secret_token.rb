d036452ef5ba648d03ae1de465e12f3c6b6d9a25
Peter Mutascio <pmutascio@gmail.com>
2013-03-11 00:01:35 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fidelity::Application.config.secret_token = 'aea349de35b14646de07fde3513addaea3b03822351b32820cef57539f714bb622394863e6877f0be03963c4c8c1439b0968650aba71afae6117c110359b39fb'
